81ea197822caa6ebe69985a3146261a016d50f5b
akash <akashnandan99@gmail.com>
2023-03-05 19:40:03 +0530

    pilot

---
+++ main.go
@@ -0,0 +1,8 @@
+package main
+
+func main() {
+	//get user config for perfomance test
+	//loop through the config and run the daemon
+	//parallely run the plugins or In built performance tester
+	//Finally make reports for each loop/config
+}
